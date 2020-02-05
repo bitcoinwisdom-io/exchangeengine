f27f5dad1aff37c5bd53011e6c42dcc93b3050c1
bitcoinwisdom-io <42641722+bitcoinwisdom-io@users.noreply.github.com>
2020-02-05 10:33:10 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 var Book = require('./Book.js');
+var Order = require('./Order.js');
+var Trade = require('./Trade.js');
+var OrderTypes = require('./constants/orderTypes.js');
 
 exports.Book = Book;
+exports.Order = Order;
+exports.Trade = Trade;
+exports.OrderTypes = OrderTypes;
